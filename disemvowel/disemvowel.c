b3e5d565c4afc7d4748cce86b1045d07441bd0e1
TyBeas <beasl110@morris.umn.edu>
2025-04-19 19:59:20 -0500

    Making files excecutable for tests

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -3,7 +3,7 @@
 
 #include "disemvowel.h"
 
-int vowelCounter(char *str) {
+int vowelCounter(char *str){
   int i, numVowels = 0;
   char *vowels = (char*)calloc(11, sizeof(char));
   strcpy(vowels, "aeiouAEIOU\0");
@@ -18,14 +18,14 @@ int vowelCounter(char *str) {
 }
 
 char *disemvowel(char *str) {
-  int length, numVowels, i, j = 0;
+  int length, vowels, i, j;
   char *results;
-  char *vowels = (char*)calloc(11, sizeof(char));
+  char* vowels = (char*)calloc(11, sizeof(char));
   strcpy(vowels, "aeiouAEIOU\0");
 
-  numVowels = vowelCounter(str);  // Use vowelCounter to get the number of vowels
+  vowels = vowelCounter(str);
   length = strlen(str);
-  results = (char*)calloc(length - numVowels + 1, sizeof(char));
+  results = (char*)calloc(length - vowels + 1, sizeof(char));
 
   for (i = 0; i < length; i++) {
     if (strchr(vowels, str[i]) == NULL) {
@@ -33,7 +33,7 @@ char *disemvowel(char *str) {
       j++;
     }
   }
-  results[length - numVowels] = '\0';
+  results[length - vowels] = '\0';
   free(vowels);
   return results;
 }
